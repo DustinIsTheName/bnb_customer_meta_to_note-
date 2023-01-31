b026958a77d60d486d6749cd3932b65beb3506c8
Dustin Milam <darklordsushi@yahoo.com>
2023-01-31 18:26:46 -0500

    update skip_before_action

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,6 @@
 class WebhooksController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def save_note
     puts Colorize.magenta(params)
